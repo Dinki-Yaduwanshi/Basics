406862ecddb6fe66b1b1073feb0705a9f019b442
Dinki-Yaduwanshi <zmeet250@gmail.com>
2024-07-17 12:48:51 +0530

    Add project files.

---
+++ Numerics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            int age = 23;
+            Console.WriteLine(age);
+            Console.WriteLine(int.MaxValue);
+
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+            long bigNumber = 900000L;
+
+            double d = 3.14;
+            Console.WriteLine(d);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+
+            float f = 4.00001F;
+            Console.WriteLine(f);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+
+            decimal money = 4.8M;
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+
+
+            Console.WriteLine(bigNumber);
+            Console.ReadLine();
+        }
+    }
+}
+
